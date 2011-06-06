7fcde2954b39cef8d89b14363e4a4cb928db76ae
cliff <wall72@changho-iui-MacBook-Pro.local>
2011-06-06 20:19:28 +0900

    Initial Commit

---
+++ FruitsNINE/FruitsNINEAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  FruitsNINEAppDelegate.h
+//  FruitsNINE
+//
+//  Created by cliff on 11. 6. 6..
+//  Copyright 2011 타임교육. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FruitsNINEAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ FruitsNINE/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  FruitsNINE
+//
+//  Created by cliff on 11. 6. 6..
+//  Copyright 2011 타임교육. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
